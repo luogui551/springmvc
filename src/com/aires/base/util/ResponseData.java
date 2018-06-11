00dc2950ae6c733e118be0f2b7e1817559e4d35c
aires <aires@aires-pc>
2018-06-11 11:35:30 +0800

    简单优化

--- src/com/aires/base/util/ResponseData.java
+++ src/com/aires/base/util/ResponseData.java
@@ -5,35 +5,74 @@ package com.aires.base.util;
  *
  */
 public class ResponseData {
-	private boolean success;
+	/*
+	 * 成功/失败
+	 */
+	private final boolean success;
+	/*
+	 * 一般用于前端提示的德信息
+	 */
 	private String msg;
+	/*
+	 * 返回到前端的额外数据（如新增时返回数据id）
+	 */
 	private Object value;
+	/**
+	 * 返回成功的对象（不包含消息和数据）
+	 * @return
+	 */
+	public static ResponseData success(){
+		return new ResponseData(true, "", "");
+	}
+	/**
+	 * 返回成功的对象（不包含消息， 包含指定数据）
+	 * @param value 数据对象
+	 * @return
+	 */
+//	public static ResponseData success(Object value){
+//		return new ResponseData(true, "", value);
+//	}
+	/**
+	 * 返回失败的对象（不包含消息和数据）
+	 * @return
+	 */
+	public static ResponseData fail(){
+		return new ResponseData(false, "", "");
+	}
+	/**
+	 * 返回失败的对象（包含消息，不包含数据）
+	 * @param msg
+	 * @return
+	 */
+	public static ResponseData fail(String msg){
+		return new ResponseData(false, msg, "");
+	}
+	/**
+	 * 为当前设置消息
+	 * @param msg
+	 * @return
+	 */
+	public ResponseData msg(String msg){
+		this.msg = msg;
+		return this;
+	}
+	/**
+	 * 为当前对象设置数据
+	 * @param value
+	 * @return
+	 */
+	public ResponseData val(Object value){
+		this.value = value;
+		return this;
+	}
 	
-	public ResponseData() {
-		this(null);
-	}
-	/**
-	 * 成功
-	 * @param msg 返回信息
-	 */
-	public ResponseData(String msg) {
-		this(true, msg);
-	}
-	/**
-	 * 成功
-	 * @param msg  返回信息
-	 * @param value  返回数据
-	 */
-	public ResponseData(String msg, Object value) {
-		this(true, msg, value);
-	}
 	/**
 	 * 
 	 * @param success 是否成功
 	 * @param msg  返回信息
 	 */
 	public ResponseData(boolean success, String msg) {
-		this(success, msg, null);
+		this(success, msg, "");
 	}
 	/**
 	 * 
@@ -42,7 +81,6 @@ public class ResponseData {
 	 * @param value  返回数据
 	 */
 	public ResponseData(boolean success, String msg, Object value) {
-		super();
 		this.success = success;
 		this.msg = msg;
 		this.value = value;
@@ -50,19 +88,10 @@ public class ResponseData {
 	public boolean isSuccess() {
 		return success;
 	}
-	public void setSuccess(boolean success) {
-		this.success = success;
-	}
 	public String getMsg() {
 		return msg;
 	}
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
 	public Object getValue() {
 		return value;
-	}
-	public void setValue(Object value) {
-		this.value = value;
 	}	
 }
